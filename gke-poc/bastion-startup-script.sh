948358bddb84cc1457d4b6981b23f9581e0b1824
StefeeB <s.bozovic@levi9.com>
2024-01-10 13:37:33 +0100

    README.md finalized bastion-startup-script.sh modified to prevent prompts

--- gke-poc/bastion-startup-script.sh
+++ gke-poc/bastion-startup-script.sh
@@ -1,5 +1,9 @@
+sudo apt-mark hold openssh-server
+sudo sed "s/.*nrconf{restart}.*/\$nrconf{restart} = \'a\';/" /etc/needrestart/needrestart.conf > tmp-nr.conf &&
+mv tmp-nr.conf /etc/needrestart/needrestart.conf
+
 sudo apt update && sudo apt upgrade -y
-sudo apt install tinyproxy -y # > startup_log.txt
+sudo apt install tinyproxy -y > startup_log.txt
 
 sudo sed '/^Allow 127.0.0.1$/a\
 Allow localhost
